f7d7c6b0dca3965e1b94b3a827e7d143a2fd8f45
Angie Jones <jones.angie@gmail.com>
2024-08-28 18:57:17 +0000

    setup

--- src/main/java/interactions/DropdownMenus.java
+++ src/main/java/interactions/DropdownMenus.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class DropdownMenus extends Main {
 
+  @SuppressWarnings("unused")
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
