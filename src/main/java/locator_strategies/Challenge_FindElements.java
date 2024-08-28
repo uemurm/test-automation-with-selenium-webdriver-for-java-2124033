217b8ca0cc7f45a68162da6e6b3376dc255bd415
Angie Jones <jones.angie@gmail.com>
2024-08-28 02:55:24 +0000

    setup

---
+++ src/main/java/locator_strategies/Challenge_FindElements.java
@@ -0,0 +1,18 @@
+package locator_strategies;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import setup.Main;
+
+public class Challenge_FindElements extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/formPage.html");
+
+    // TODO: add your solution here
+
+
+    driver.quit();
+  }
+}
