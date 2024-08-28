d89fe3aa701d6e6d461140a5187632db4108423f
Angie Jones <jones.angie@gmail.com>
2024-08-28 02:26:05 +0000

    solution

--- src/main/java/locator_strategies/FindingMultipleElements.java
+++ src/main/java/locator_strategies/FindingMultipleElements.java
@@ -11,7 +11,10 @@ public class FindingMultipleElements extends Main{
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
 
+    List<WebElement> radioButtons = driver.findElements(By.cssSelector("input[type='radio']"));
     
+    System.out.println(radioButtons.size());
+
     driver.quit();
   }
 
