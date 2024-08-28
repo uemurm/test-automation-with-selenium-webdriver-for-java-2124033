ab9315c3d5c52b69959cad6959293c800b62ea5b
Angie Jones <jones.angie@gmail.com>
2024-08-28 03:39:32 +0000

    setup

---
+++ src/main/java/interactions/ClickableElements.java
@@ -0,0 +1,17 @@
+package interactions;
+
+import org.openqa.selenium.By;
+
+import setup.Main;
+
+public class ClickableElements extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+    
+
+    driver.quit();
+  }
+  
+}
