30a112e1f254dfb16a191b996dabf69ed904a799
Angie Jones <jones.angie@gmail.com>
2024-08-28 02:09:13 +0000

    setup

---
+++ src/main/java/locator_strategies/FindingAnElement.java
@@ -0,0 +1,17 @@
+package locator_strategies;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import setup.Main;
+
+public class FindingAnElement extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/formPage.html");
+
+    
+
+    driver.quit();
+  }
+}
