ce6ea81016e7c6782bdd94e2635dd9b0b5037eec
Angie Jones <jones.angie@gmail.com>
2024-08-28 19:29:54 +0000

    solution

--- src/main/java/interactions/FileUploads.java
+++ src/main/java/interactions/FileUploads.java
@@ -10,7 +10,9 @@ public class FileUploads extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-
+    String filePath = "/workspaces/test-automation-with-selenium-webdriver-for-java-2124033/resources/file.txt";
+    WebElement uploadElement = driver.findElement(By.name("my-file"));
+    uploadElement.sendKeys(filePath);
 
     driver.quit();
   }
