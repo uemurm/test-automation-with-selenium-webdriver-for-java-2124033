903f94e2617104ee6a4a8bf0449f90761e994f89
Angie Jones <jones.angie@gmail.com>
2024-08-28 02:18:29 +0000

    setup

---
+++ src/main/java/locator_strategies/FindingMultipleElements.java
@@ -0,0 +1,18 @@
+package locator_strategies;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import setup.Main;
+import java.util.List;
+
+public class FindingMultipleElements extends Main{
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/formPage.html");
+
+    
+    driver.quit();
+  }
+
+}
