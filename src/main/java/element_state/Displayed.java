083be6eccc7fe2fbec18554fa1e61de46612b15e
Angie Jones <jones.angie@gmail.com>
2024-08-28 20:06:36 +0000

    set up

---
+++ src/main/java/element_state/Displayed.java
@@ -0,0 +1,15 @@
+package element_state;
+
+import setup.Main;
+import org.openqa.selenium.By;
+
+public class Displayed extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+    
+
+    driver.quit();
+  }
+}
