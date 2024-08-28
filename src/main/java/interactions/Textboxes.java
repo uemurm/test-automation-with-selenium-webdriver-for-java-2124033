137ccbb8d6de3c60dd6145b399630d5a3e8c1a1e
Angie Jones <jones.angie@gmail.com>
2024-08-28 03:24:59 +0000

    setup

---
+++ src/main/java/interactions/Textboxes.java
@@ -0,0 +1,17 @@
+package interactions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import setup.Main;
+
+public class Textboxes extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/formPage.html");
+
+    
+
+    driver.quit();
+  }
+}
