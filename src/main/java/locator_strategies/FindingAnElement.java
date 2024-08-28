b173a4f3b03418fb8d0a98ded74ca08b9ddac69e
Angie Jones <jones.angie@gmail.com>
2024-08-28 02:12:19 +0000

    solution

--- src/main/java/locator_strategies/FindingAnElement.java
+++ src/main/java/locator_strategies/FindingAnElement.java
@@ -10,7 +10,7 @@ public class FindingAnElement extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
 
-    
+    WebElement emailTextBox = driver.findElement(By.id("email"));
 
     driver.quit();
   }
