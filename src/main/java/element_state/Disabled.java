fe7fc662d070050bd11a0739a43f7525d38423ab
Angie Jones <jones.angie@gmail.com>
2024-08-28 20:11:05 +0000

    setup

---
+++ src/main/java/element_state/Disabled.java
@@ -0,0 +1,16 @@
+package element_state;
+
+import setup.Main;
+import org.openqa.selenium.By;
+
+public class Disabled extends Main {
+
+  @SuppressWarnings("unused")
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+    
+
+    driver.quit();
+  }
+}
