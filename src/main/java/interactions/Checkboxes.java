b24a51b6b7a8554619ad18011184ad510ad2cf53
Angie Jones <jones.angie@gmail.com>
2024-08-28 03:49:19 +0000

    setup

---
+++ src/main/java/interactions/Checkboxes.java
@@ -0,0 +1,15 @@
+package interactions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import setup.Main;
+
+public class Checkboxes extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+    
+    driver.quit();
+  }
+}
