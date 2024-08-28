c51f927b1c3588b1c80e99df3f72037bcaef0710
Angie Jones <jones.angie@gmail.com>
2024-08-28 20:15:56 +0000

    setup

---
+++ src/main/java/element_state/Challenge_DetectVisibility.java
@@ -0,0 +1,16 @@
+package element_state;
+
+import setup.Main;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Challenge_DetectVisibility extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/coordinates_tests/page_with_hidden_element.html");
+
+    
+
+    driver.quit();
+  }
+}
