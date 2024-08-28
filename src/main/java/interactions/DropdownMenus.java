22061a94c623442af90280303f50f0f86a633d9f
Angie Jones <jones.angie@gmail.com>
2024-08-28 18:56:21 +0000

    solution

--- src/main/java/interactions/DropdownMenus.java
+++ src/main/java/interactions/DropdownMenus.java
@@ -5,14 +5,32 @@ import setup.Main;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
 import java.util.List;
 
 public class DropdownMenus extends Main {
 
+
+  @SuppressWarnings("unused")
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
+    driver.findElement(By.name("my-select"));
+    Select dropdown = new Select(driver.findElement(By.name("my-select")));
+    dropdown.selectByValue("2");
+    dropdown.selectByVisibleText("One");
+    dropdown.selectByIndex(0);
 
+    WebElement selectedOption = dropdown.getFirstSelectedOption();
+    List<WebElement> selectedOptions = dropdown.getAllSelectedOptions();
+
+    /*
+     * Deselect only works on multi-select dropdown menus
+     * 
+     * dropdown.deselectByValue("2");
+     * dropdown.deselectByVisibleText("One");
+     * dropdown.deselectByIndex(0); 
+     */
 
     driver.quit();
   }
