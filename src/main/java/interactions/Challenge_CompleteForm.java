575bfaad375e55c4a834a6a44a40922b5d264bdc
Angie Jones <jones.angie@gmail.com>
2024-08-28 19:43:50 +0000

    setup

---
+++ src/main/java/interactions/Challenge_CompleteForm.java
@@ -0,0 +1,19 @@
+package interactions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import setup.Main;
+
+public class Challenge_CompleteForm extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+    // TODO: add your solution here
+
+
+    driver.quit();
+  }
+}
