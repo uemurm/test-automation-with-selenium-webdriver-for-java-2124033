420358f66787ba9e09361d8b0d64ebdfd5f575ae
Angie Jones <jones.angie@gmail.com>
2024-08-28 03:10:09 +0000

    solution

--- src/main/java/locator_strategies/FindingAnElement.java
+++ src/main/java/locator_strategies/FindingAnElement.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.WebElement;
 import setup.Main;
 
 public class FindingAnElement extends Main {
-
+  
+  @SuppressWarnings("unused")
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
 
