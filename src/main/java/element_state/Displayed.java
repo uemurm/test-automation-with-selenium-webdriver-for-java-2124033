1227846bf9898d30d4987684e97b55200b2112bf
Angie Jones <jones.angie@gmail.com>
2024-08-28 20:08:15 +0000

    solution

--- src/main/java/element_state/Displayed.java
+++ src/main/java/element_state/Displayed.java
@@ -5,10 +5,11 @@ import org.openqa.selenium.By;
 
 public class Displayed extends Main {
 
+  @SuppressWarnings("unused")
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-    
+    boolean isVisible = driver.findElement(By.name("my-hidden")).isDisplayed();
 
     driver.quit();
   }
