cecea4198a0b1dd9b107086c3a7eaa632cc87a05
Angie Jones <jones.angie@gmail.com>
2024-08-28 03:40:46 +0000

    solution

--- src/main/java/interactions/ClickableElements.java
+++ src/main/java/interactions/ClickableElements.java
@@ -8,10 +8,11 @@ public class ClickableElements extends Main {
 
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
     
+    //driver.findElement(By.xpath("//button[text()='Submit']")).click();
+    driver.findElement(By.xpath("//a[normalize-space(text())='Return to index']")).click();
 
     driver.quit();
   }
-  
+
 }
