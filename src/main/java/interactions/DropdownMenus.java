d54af525340f8472878ec95b3c22ade41d399463
Angie Jones <jones.angie@gmail.com>
2024-08-28 18:15:51 +0000

    setup

---
+++ src/main/java/interactions/DropdownMenus.java
@@ -0,0 +1,20 @@
+package interactions;
+
+import setup.Main;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import java.util.List;
+
+public class DropdownMenus extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+
+
+    driver.quit();
+  }
+  
+}
