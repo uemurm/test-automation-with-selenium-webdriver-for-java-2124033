59175c15fd00c2ad5cc55d56906163b7ad486f2f
Angie Jones <jones.angie@gmail.com>
2024-08-28 20:12:29 +0000

    solution

--- src/main/java/element_state/Disabled.java
+++ src/main/java/element_state/Disabled.java
@@ -9,7 +9,7 @@ public class Disabled extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-    
+    boolean isEnabled = driver.findElement(By.name("my-disabled")).isEnabled();
 
     driver.quit();
   }
