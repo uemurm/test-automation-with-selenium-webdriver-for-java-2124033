650a9a8220a79d5616fbf193f36b7096ab0f1d2a
Angie Jones <jones.angie@gmail.com>
2024-08-28 21:21:26 +0000

    solution

--- src/main/java/wait_strategies/FluentWaits.java
+++ src/main/java/wait_strategies/FluentWaits.java
@@ -9,15 +9,20 @@ import org.openqa.selenium.support.ui.FluentWait;
 import java.time.Duration;
 import setup.Main;
 
-public class FluentWaits extends Main
-{
+public class FluentWaits extends Main {
 
   @SuppressWarnings("unused")
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
     driver.findElement(By.id("adder")).click();
 
+    var wait = new FluentWait<>(driver)
+        .withTimeout(Duration.ofSeconds(5))
+        .pollingEvery(Duration.ofMillis(200))
+        .ignoring(NoSuchElementException.class);
 
+    WebElement box = wait.until(
+        ExpectedConditions.presenceOfElementLocated(By.id("box0")));
 
     driver.quit();
   }
