55cad7c744ddc8ff9958ccd0de6890e81a18a19d
Angie Jones <jones.angie@gmail.com>
2024-08-28 20:37:19 +0000

    solution

--- src/main/java/wait_strategies/ImplicitWait.java
+++ src/main/java/wait_strategies/ImplicitWait.java
@@ -8,7 +8,7 @@ public class ImplicitWait extends Main {
 
   public static void main(String[] args) {
     
-    
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
     driver.findElement(By.id("reveal")).click();
     driver.findElement(By.id("revealed")).sendKeys("I see you");
