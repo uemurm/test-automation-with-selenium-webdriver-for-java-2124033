f9f04acceac9ffaf35fe8dfd560c37f8fe49dac0
Angie Jones <jones.angie@gmail.com>
2024-08-28 19:48:47 +0000

    solution

--- src/main/java/interactions/Challenge_CompleteForm.java
+++ src/main/java/interactions/Challenge_CompleteForm.java
@@ -11,8 +11,35 @@ public class Challenge_CompleteForm extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-    // TODO: add your solution here
+    // enter text into input of type text
+    driver.findElement(By.id("my-text-id")).sendKeys("username");
 
+    // enter text into input of type password
+    driver.findElement(By.name("my-password")).sendKeys("passw0rd");
+
+    // enter text into textarea
+    driver.findElement(By.name("my-textarea")).sendKeys("I am learning Selenium WebDriver");
+
+    // select from dropdown
+    new Select(driver.findElement(By.name("my-select"))).selectByValue("3");
+
+    // deselect checkbox
+    WebElement checkedCheckbox = driver.findElement(By.id("my-check-1"));
+    if(checkedCheckbox.isSelected()){
+        checkedCheckbox.click();
+    }
+
+    // select checkbox
+    WebElement defaultCheckbox = driver.findElement(By.id("my-check-2"));
+    if(!defaultCheckbox.isSelected()){
+        defaultCheckbox.click();
+    }
+
+    // select radio button
+    driver.findElement(By.id("my-radio-2")).click();
+
+    // click button
+    driver.findElement(By.xpath("//button[text()='Submit']")).click();
 
     driver.quit();
   }
