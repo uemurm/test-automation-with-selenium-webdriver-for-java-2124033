b523962b059d8e11ba96cbb185c2ff94ff342f8a
Angie Jones <jones.angie@gmail.com>
2024-08-28 20:08:54 +0000

    setup

--- src/main/java/element_state/Displayed.java
+++ src/main/java/element_state/Displayed.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 
 public class Displayed extends Main {
 
+  @SuppressWarnings("unused")
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
