55349ac00e1283c46164a4625bb1b1fec36f0e0e
Angie Jones <jones.angie@gmail.com>
2024-08-28 03:25:53 +0000

    solution

--- src/main/java/interactions/Textboxes.java
+++ src/main/java/interactions/Textboxes.java
@@ -10,7 +10,10 @@ public class Textboxes extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
 
-    
+    WebElement textbox = driver.findElement(By.name("id-name1"));
+    textbox.getText();
+    textbox.clear();
+    textbox.sendKeys("angie");
 
     driver.quit();
   }
