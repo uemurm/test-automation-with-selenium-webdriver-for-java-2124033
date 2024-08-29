789062f2a9c0b7a1e6d612a609f7b168fbe1e22d
Angie Jones <jones.angie@gmail.com>
2024-08-29 13:12:20 +0000

    solution

--- src/main/java/browser_interactions/Tabs.java
+++ src/main/java/browser_interactions/Tabs.java
@@ -10,7 +10,18 @@ public class Tabs extends Main {
     driver.get("https://www.selenium.dev/selenium/web/window_switching_tests/page_with_frame.html");
 
     driver.findElement(By.id("a-link-that-opens-a-new-window")).click();
+    var windows = driver.getWindowHandles();
+    System.out.println(windows);
 
+    String originalWindow = driver.getWindowHandle();
+    for (String window : windows) {
+      if (!window.equals(originalWindow)) {
+        driver.switchTo().window(window);
+        break;
+      }
+    }
+
+    System.out.println(driver.getTitle());
 
     driver.quit();
   }
