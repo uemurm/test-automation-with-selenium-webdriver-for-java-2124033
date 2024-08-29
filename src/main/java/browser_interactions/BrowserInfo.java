c6da067095cca9b3f023836926695e4568d62f9d
Angie Jones <jones.angie@gmail.com>
2024-08-29 12:22:17 +0000

    setup

---
+++ src/main/java/browser_interactions/BrowserInfo.java
@@ -0,0 +1,14 @@
+package browser_interactions;
+
+import setup.Main;
+
+public class BrowserInfo extends Main {
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+
+
+    driver.quit();
+  }
+}
