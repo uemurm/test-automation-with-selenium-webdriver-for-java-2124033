7fc63095824602211ea49762ce10f10c18b410a9
Angie Jones <jones.angie@gmail.com>
2024-08-29 12:23:17 +0000

    solution

--- src/main/java/browser_interactions/BrowserInfo.java
+++ src/main/java/browser_interactions/BrowserInfo.java
@@ -7,7 +7,10 @@ public class BrowserInfo extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-
+    String title = driver.getTitle();
+    String url = driver.getCurrentUrl();
+    System.out.println(title);
+    System.out.println(url);
 
     driver.quit();
   }
