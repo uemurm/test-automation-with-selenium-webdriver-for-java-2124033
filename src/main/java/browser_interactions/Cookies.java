c0d835e7c400ae16df9bdb17175404bb25a57ee6
Angie Jones <jones.angie@gmail.com>
2024-08-30 18:23:19 +0000

    solution

--- src/main/java/browser_interactions/Cookies.java
+++ src/main/java/browser_interactions/Cookies.java
@@ -9,7 +9,14 @@ public class Cookies extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
+    Cookie cookie = new Cookie("theme", "light");
+    driver.manage().addCookie(cookie);
 
+    Cookie themeCookie = driver.manage().getCookieNamed("theme");
+    System.out.println(themeCookie.getDomain());
+
+    driver.manage().deleteCookie(themeCookie);
+    System.out.println(driver.manage().getCookies().size());
 
     driver.quit();
   }
