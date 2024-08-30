1f5401b2cb80916f756dc564940075807da66313
Angie Jones <jones.angie@gmail.com>
2024-08-30 18:01:14 +0000

    solution

--- src/main/java/browser_interactions/Alerts.java
+++ src/main/java/browser_interactions/Alerts.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 import setup.Main;
 
 public class Alerts extends Main {
-  
+
   public static void main(String[] args) {
     alerts();
     confirmations();
@@ -19,31 +19,42 @@ public class Alerts extends Main {
     driver.quit();
   }
 
-  public static void alerts(){
+  public static void alerts() {
     driver.get("https://www.selenium.dev/selenium/web/alerts.html");
     driver.findElement(By.id("alert")).click();
 
+    Alert alert = driver.switchTo().alert();
+    String message = alert.getText();
 
-  } 
+    System.out.println(message);
+
+    alert.accept();
+  }
 
   public static void confirmations() {
-      driver.get("https://www.selenium.dev/selenium/web/alerts.html");
-      driver.findElement(By.id("confirm")).click();
+    driver.get("https://www.selenium.dev/selenium/web/alerts.html");
+    driver.findElement(By.id("confirm")).click();
 
-
+    Alert confirmation = driver.switchTo().alert();
+    // confirmation.accept(); // OK
+    confirmation.dismiss(); // Cancel
   }
 
-  public static void prompts(){
-      driver.get("https://www.selenium.dev/selenium/web/alerts.html");
-      driver.findElement(By.id("prompt")).click();
+  public static void prompts() {
+    driver.get("https://www.selenium.dev/selenium/web/alerts.html");
+    driver.findElement(By.id("prompt")).click();
 
-
+    Alert prompt = driver.switchTo().alert();
+    prompt.sendKeys("something");
+    prompt.accept();
   }
 
-  public static void waits(){
-      driver.get("https://www.selenium.dev/selenium/web/alerts.html");
-      driver.findElement(By.id("slow-alert")).click();
+  public static void waits() {
+    driver.get("https://www.selenium.dev/selenium/web/alerts.html");
+    driver.findElement(By.id("slow-alert")).click();
 
-
+    var wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+    Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+    alert.accept();
   }
 }
