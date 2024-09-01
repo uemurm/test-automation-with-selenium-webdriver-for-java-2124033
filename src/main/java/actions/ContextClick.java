8649c23e64139acee75297d9b2ba76ffb1b4efee
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:07:20 +0000

    setup

---
+++ src/main/java/actions/ContextClick.java
@@ -0,0 +1,17 @@
+package actions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+
+import setup.Main;
+
+public class ContextClick extends Main {
+  
+  public static void main(String[] args) {
+    driver.get("https://selenium.dev/selenium/web/mouse_interaction.html");
+        
+
+
+    driver.quit();
+  }
+}
