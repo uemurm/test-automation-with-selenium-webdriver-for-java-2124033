df4b3bbb672ba5229f34e8afbdd35bb280676eb3
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:45:31 +0000

    solution

--- src/main/java/page_objects/LoginPage.java
+++ src/main/java/page_objects/LoginPage.java
@@ -5,10 +5,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-  
+
   private final By usernameField = By.id("username-field");
   private final By passwordField = By.id("password-field");
   private final By loginButton = By.id("login-form-submit");
 
-  
+  private final WebDriver driver;
+
+  public LoginPage(WebDriver driver) {
+    this.driver = driver;
+  }
+
+  public void setUsername(String username) {
+    driver.findElement(usernameField).sendKeys(username);
+  }
+
+  public void setPassword(String password) {
+    driver.findElement(passwordField).sendKeys(password);
+  }
+
+  public String clickLoginButton() {
+    driver.findElement(loginButton).click();
+    Alert alert = driver.switchTo().alert();
+    String message = alert.getText();
+    alert.accept();
+    return message;
+  }
+
+  public String login(String username, String password) {
+    setUsername(username);
+    setPassword(password);
+    return clickLoginButton();
+  }
 }
