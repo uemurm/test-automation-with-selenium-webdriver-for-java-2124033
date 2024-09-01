7d79d687293ed64e13d5117770a5d8a5c74e9b65
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:22:25 +0000

    solution

--- src/main/java/actions/CopyAndPaste.java
+++ src/main/java/actions/CopyAndPaste.java
@@ -8,11 +8,31 @@ import org.openqa.selenium.interactions.Actions;
 import setup.Main;
 
 public class CopyAndPaste extends Main {
-  
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
 
+    var field1 = driver.findElement(By.id("withText"));
+    var field2 = driver.findElement(By.id("emptyTextArea"));
 
+    Keys cmdCtrl = Platform.getCurrent().is(Platform.MAC) ? Keys.COMMAND : Keys.CONTROL;
+
+    Actions actions = new Actions(driver);
+
+    // Copy
+    actions.click(field1)
+        .keyDown(cmdCtrl)
+        .sendKeys("a")
+        .sendKeys("c")
+        .keyUp(cmdCtrl)
+        .perform();
+
+    // Paste
+    actions.click(field2)
+        .keyDown(cmdCtrl)
+        .sendKeys("v")
+        .keyUp(cmdCtrl)
+        .perform();
 
     driver.quit();
   }
