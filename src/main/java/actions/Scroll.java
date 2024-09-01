d53ed36bdec35609734f6f4d38fb7c19bd0f7ecb
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:12:24 +0000

    setup

---
+++ src/main/java/actions/Scroll.java
@@ -0,0 +1,22 @@
+package actions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+
+import setup.Main;
+
+public class Scroll extends Main {
+
+  public static void main(String[] args) {
+
+    // Scroll to element
+    driver.get("https://www.selenium.dev/selenium/web/scroll.html");
+
+
+    //Scroll by amount
+    driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/page_with_tall_frame.html");
+
+
+    driver.quit();
+  }
+}
