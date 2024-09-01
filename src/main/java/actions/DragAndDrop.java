8e6cf06bab374b89c82736db3fbe21bf87fbfb97
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:17:40 +0000

    solution

--- src/main/java/actions/DragAndDrop.java
+++ src/main/java/actions/DragAndDrop.java
@@ -10,8 +10,12 @@ public class DragAndDrop extends Main {
 
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/draggableLists.html");
-    
 
+    WebElement leftItem = driver.findElement(By.id("leftitem-1"));
+    WebElement rightItem = driver.findElement(By.id("rightitem-1"));
+    new Actions(driver).dragAndDrop(leftItem, rightItem).perform();
+    var rightItems = driver.findElements(By.cssSelector("#sortable2 li"));
+    rightItems.forEach(e -> System.out.println(e.getText()));
 
     driver.quit();
   }
