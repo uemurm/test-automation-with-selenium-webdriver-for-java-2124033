9b444bc362ca21673b6b296f3b19b75fb48077d1
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:07:57 +0000

    solution

--- src/main/java/actions/ContextClick.java
+++ src/main/java/actions/ContextClick.java
@@ -6,11 +6,13 @@ import org.openqa.selenium.interactions.Actions;
 import setup.Main;
 
 public class ContextClick extends Main {
-  
+
   public static void main(String[] args) {
     driver.get("https://selenium.dev/selenium/web/mouse_interaction.html");
-        
 
+    var input = driver.findElement(By.id("clickable"));
+    new Actions(driver).contextClick(input).perform();
+    System.out.println(driver.findElement(By.id("click-status")).getText());
 
     driver.quit();
   }
