7a4c61282569b35e89092b02ce6f23520e9568d7
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:30:34 +0000

    solution

--- src/main/java/actions/Challenge_MultipleKeyPresses.java
+++ src/main/java/actions/Challenge_MultipleKeyPresses.java
@@ -10,7 +10,13 @@ public class Challenge_MultipleKeyPresses extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/keyboard_shortcut.html");
 
-
+    new Actions(driver)
+        .keyDown(Keys.CONTROL)
+        .keyDown(Keys.SHIFT)
+        .sendKeys("1")
+        .keyUp(Keys.SHIFT)
+        .keyUp(Keys.CONTROL)
+        .perform();
 
     driver.quit();
   }
