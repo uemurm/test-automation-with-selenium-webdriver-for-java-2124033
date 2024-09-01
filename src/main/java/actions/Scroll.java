fdeed70559682e18a44ac74ae16230c818c94234
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:13:36 +0000

    solution

--- src/main/java/actions/Scroll.java
+++ src/main/java/actions/Scroll.java
@@ -11,11 +11,14 @@ public class Scroll extends Main {
 
     // Scroll to element
     driver.get("https://www.selenium.dev/selenium/web/scroll.html");
+    var line9 = driver.findElement(By.id("line9"));
+    System.out.println(line9.isDisplayed());
+    new Actions(driver).scrollToElement(line9).perform();
 
-
-    //Scroll by amount
+    // Scroll by amount
     driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/page_with_tall_frame.html");
-
+    var height = driver.findElement(By.tagName("body")).getSize().getHeight();
+    new Actions(driver).scrollByAmount(0, height).perform();
 
     driver.quit();
   }
