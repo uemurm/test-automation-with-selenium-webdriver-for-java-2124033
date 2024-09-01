aaa8f7876d69470d9c75148aaeb5fe3f24fc4ec5
Angie Jones <jones.angie@gmail.com>
2024-09-01 17:59:12 +0000

    solution

--- src/main/java/actions/Hover.java
+++ src/main/java/actions/Hover.java
@@ -6,11 +6,15 @@ import org.openqa.selenium.interactions.Actions;
 import setup.Main;
 
 public class Hover extends Main {
-  
+
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/mouseOver.html");
 
+    var redBox = driver.findElement(By.id("redbox"));
+    System.out.println(redBox.getCssValue("background-color"));
 
+    new Actions(driver).moveToElement(redBox).perform();
+    System.out.println(redBox.getCssValue("background-color"));
 
     driver.quit();
   }
