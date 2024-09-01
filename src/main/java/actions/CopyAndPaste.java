eeb843586c23f7749cf0eff5b7ec2e597b4f1eb1
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:21:19 +0000

    setup

---
+++ src/main/java/actions/CopyAndPaste.java
@@ -0,0 +1,19 @@
+package actions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.interactions.Actions;
+
+import setup.Main;
+
+public class CopyAndPaste extends Main {
+  
+  public static void main(String[] args){
+    driver.get("https://www.selenium.dev/selenium/web/formPage.html");
+
+
+
+    driver.quit();
+  }
+}
