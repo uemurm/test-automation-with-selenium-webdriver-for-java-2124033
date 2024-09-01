8b5d7155a5315a2a51316bddc41ab88e7aa72fc1
Angie Jones <jones.angie@gmail.com>
2024-09-01 17:15:22 +0000

    solution

--- src/main/java/browser_interactions/Challenge_AlertInNewTab.java
+++ src/main/java/browser_interactions/Challenge_AlertInNewTab.java
@@ -9,11 +9,28 @@ import java.time.Duration;
 import setup.Main;
 
 public class Challenge_AlertInNewTab extends Main {
-  
+
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/alerts.html");
 
+    driver.findElement(By.id("open-window-with-onload-alert")).click();
 
+    var tabs = driver.getWindowHandles();
+    var currentTab = driver.getWindowHandle();
+
+    for (String tab : tabs) {
+      driver.switchTo().window(tab);
+      if (!currentTab.equals(driver.getWindowHandle())) {
+        break;
+      }
+    }
+
+    new WebDriverWait(driver, Duration.ofSeconds(2))
+        .until(ExpectedConditions.alertIsPresent());
+
+    Alert alert = driver.switchTo().alert();
+    System.out.println(alert.getText());
+    alert.accept();
 
     driver.quit();
   }
