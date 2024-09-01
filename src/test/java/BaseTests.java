612d5847fbc13a2f7be1defff718d43a3972b6ff
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:54:28 +0000

    setup

---
+++ src/test/java/BaseTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+
+public class BaseTests {
+
+
+}
