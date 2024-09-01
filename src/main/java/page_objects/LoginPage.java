13a6c116f9373ec3467dab90499a77def903442d
Angie Jones <jones.angie@gmail.com>
2024-09-01 18:44:34 +0000

    setup

--- src/main/java/page_objects/LoginPage.java
+++ src/main/java/page_objects/LoginPage.java
@@ -1,13 +1,14 @@
 package page_objects;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-@SuppressWarnings("unused")
 public class LoginPage {
   
   private final By usernameField = By.id("username-field");
   private final By passwordField = By.id("password-field");
   private final By loginButton = By.id("login-form-submit");
 
+  
 }
