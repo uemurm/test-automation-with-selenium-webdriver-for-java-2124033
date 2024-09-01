ad1a0e5d6ff5a8af027899e7037952b88cbc8584
Angie Jones <jones.angie@gmail.com>
2024-09-01 19:02:40 +0000

    setup

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import page_objects.LoginPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTests {
+
+
+}
