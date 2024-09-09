1f0f8c5afc3139b7e5dd7399dd92c04dbeb9ee11
Angie Jones <jones.angie@gmail.com>
2024-09-09 17:03:22 +0000

    setup

--- src/main/java/setup/Main.java
+++ src/main/java/setup/Main.java
@@ -1,5 +1,11 @@
 package setup;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+
 public class Main {
 
 
