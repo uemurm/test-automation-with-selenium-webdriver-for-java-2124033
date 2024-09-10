98b8e365e1d516fa2ee4d6ecc7b58ae137f8194c
Angie Jones <jones.angie@gmail.com>
2024-09-10 20:09:28 +0000

    setup

--- src/main/java/interactions/Challenge_CompleteForm.java
+++ src/main/java/interactions/Challenge_CompleteForm.java
@@ -11,8 +11,17 @@ public class Challenge_CompleteForm extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-    // TODO: add your solution here
-
+    /*
+     * TODO: add your solution here
+     * Enter “username” in the Text input field
+     * Enter passw0rd in the Password field
+     * Write “I am learning Selenium WebDriver” in the Textarea field
+     * Select the Three option from the dropdown
+     * Deselect Checked checkbox
+     * Select Default checkbox
+     * Select Default radio
+     * And click the Submit button
+     */ 
 
     driver.quit();
   }
