c1412385436f9da3ed9365c01ac8c4cf283cb6cb
Angie Jones <jones.angie@gmail.com>
2024-09-10 20:37:55 +0000

    setup

--- src/main/java/element_state/Challenge_DetectVisibility.java
+++ src/main/java/element_state/Challenge_DetectVisibility.java
@@ -9,7 +9,13 @@ public class Challenge_DetectVisibility extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/coordinates_tests/page_with_hidden_element.html");
 
-    
+    /*
+     * TODO: add your solution here
+     * find the box element
+     * check if the element is visible
+     * if the element is visible, click it
+     * if the element is not visible, print “box is not visible”
+     */
 
     driver.quit();
   }
