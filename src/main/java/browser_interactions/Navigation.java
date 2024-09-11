c7a97b04ea248411fca3da25a812cfcff5566968
Angie Jones <jones.angie@gmail.com>
2024-09-11 16:47:58 +0000

    setup

--- src/main/java/browser_interactions/Navigation.java
+++ src/main/java/browser_interactions/Navigation.java
@@ -8,8 +8,12 @@ public class Navigation extends Main {
 
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/clicks.html");
+    System.out.println("on page: " + driver.getTitle());
 
+    driver.findElement(By.id("twoClientRects")).click();
+    System.out.println("clicked link. now on page: " + driver.getTitle());
 
+    
 
     driver.quit();
   }
