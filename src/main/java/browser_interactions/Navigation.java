76e8cfc8a5cac03fb8742f613f4734680b75eae4
Angie Jones <jones.angie@gmail.com>
2024-09-11 17:01:58 +0000

    solution

--- src/main/java/browser_interactions/Navigation.java
+++ src/main/java/browser_interactions/Navigation.java
@@ -8,14 +8,13 @@ public class Navigation extends Main {
 
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/clicks.html");
-
     System.out.println("on page: " + driver.getTitle());
 
     driver.findElement(By.id("twoClientRects")).click();
     System.out.println("clicked link. now on page: " + driver.getTitle());
 
     driver.navigate().back();
-    System.out.println("went back.  on page: " + driver.getTitle());
+    System.out.println("went back. on page: " + driver.getTitle());
 
     driver.navigate().forward();
     System.out.println("went forward. on page: " + driver.getTitle());
