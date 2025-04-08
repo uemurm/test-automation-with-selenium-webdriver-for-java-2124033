e9a5407b2447ea59d9fcf0f65aef90edd621c75c
uemurm <uemurm@users.noreply.github.com>
2025-04-08 04:53:37 +0000

    Refactor DoubleClick class to demonstrate double-click actions using Selenium WebDriver

--- src/main/java/com/rock/yacht/actions_api/DoubleClick.java
+++ src/main/java/com/rock/yacht/actions_api/DoubleClick.java
@@ -1,25 +1,20 @@
 package com.rock.yacht.actions_api;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Rectangle;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-// import org.openqa.selenium.bidi.browsingcontext.BrowsingContext;
-import org.openqa.selenium.remote.RemoteWebElement;
 
 import setup.Main;
 
-public class Hover extends Main {
+public class DoubleClick extends Main {
 
   public static void main(String[] args) {
-    String url = "https://selenium.dev/selenium/web/mouseOver.html";
+    String url = "https://selenium.dev/selenium/web/mouse_interaction.html";
     driver.get(url);
 
-    var redBox = driver.findElement(By.id("redbox"));
-    System.out.println(redBox.getCssValue("background-color"));
+    var clickable = driver.findElement(By.id("clickable"));
 
-    new Actions(driver).moveToElement(redBox).perform();
-    System.out.println(redBox.getCssValue("background-color"));
+    new Actions(driver).doubleClick(clickable).perform();
+    System.out.println(driver.findElement(By.id("click-status")).getText());
 
     driver.quit();
   }
